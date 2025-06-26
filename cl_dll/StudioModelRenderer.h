e27b3cb00ca683c94cbfd0141840883d37241fba
Pacman09 <pacmanproductionsz25@gmail.com>
2025-06-26 00:03:58 -0600

    Revert "camera animations?!"
    This reverts commit aaa0fa50144e8878170a9df6b0179b907a66f263.

--- cl_dll/StudioModelRenderer.h
+++ cl_dll/StudioModelRenderer.h
@@ -350,16 +350,7 @@ public:
 
 #pragma once
 
-#include <map>
-#include <string>
-
-struct caminfo_s
-{
-	int bone = 0;
-	float flScale = 1.0f;
-};
-
-	/*
+/*
 ====================
 CStudioModelRenderer
 
@@ -531,25 +522,6 @@ public:
 	// Concatenated bone and light transforms
 	float (*m_pbonetransform)[MAXSTUDIOBONES][3][4];
 	float (*m_plighttransform)[MAXSTUDIOBONES][3][4];
-
-private:
-	Add commentMore actions void InitCamAnimInfo();
-	void UpdateCamInfo();
-
-	std::map<std::string, caminfo_s> m_CamInfos;
-	const caminfo_s* m_pCurrentCamInfo;
-	model_t* m_pCachedViewModel;
-
-	float m_basetransform[MAXSTUDIOBONES][3][4];
-
-	Vector m_vBaseCamAngles;
-	Vector m_vPrevCamAngles;
-	Vector m_vNewCamAngles;
-
-	bool m_bCalcBaseAngles = false;
-
-public:
-	Vector m_vCamAngles;
 };
 
 #endif
