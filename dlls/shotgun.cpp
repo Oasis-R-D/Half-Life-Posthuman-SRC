9113c974ac325debda0fd15168e820c732a05ac0
Pacman09 <pacmanproductionsz25@gmail.com>
2025-08-04 21:50:45 -0600

    half quack shotgun

--- dlls/shotgun.cpp
+++ dlls/shotgun.cpp
@@ -117,7 +117,7 @@ void CShotgun::PrimaryAttack()
 	Vector vecSrc = m_pPlayer->GetGunPosition();
 	Vector vecAiming = m_pPlayer->GetAutoaimVector(AUTOAIM_5DEGREES);
 	//Vector spread = pev->armorvalue == 0 ? VECTOR_CONE_5DEGREES : VECTOR_CONE_10DEGREES;
-	Vector spread = pev->armorvalue == 0 ? Vector(0.08716, 0.015, 0.08716) : Vector(0.17432, 0.01746, 0.17432);
+	Vector spread = pev->armorvalue == 0 ? VECTOR_CONE_5DEGREES : Vector(0.17432, 0.01746, 0.17432);
 	m_pPlayer->FireBullets(9, vecSrc, vecAiming, spread, 2048, BULLET_PLAYER_BUCKSHOT, 1);
 
 	if (pev->armorvalue == 0)
@@ -180,7 +180,11 @@ void CShotgun::SecondaryAttack()
 
 	Vector vecSrc = m_pPlayer->GetGunPosition();
 	Vector vecAiming = m_pPlayer->GetAutoaimVector(AUTOAIM_5DEGREES);
-	m_pPlayer->FireBullets(18, vecSrc, vecAiming, Vector(0.25, 0.02, 0.25), 2048, BULLET_PLAYER_BUCKSHOT, 1);
+	Vector spread = pev->armorvalue == 0 ? VECTOR_CONE_15DEGREES : Vector(0.25, 0.02, 0.25);
+	m_pPlayer->FireBullets(18, vecSrc, vecAiming, spread, 2048, BULLET_PLAYER_BUCKSHOT, 1);
+	
+	
+	//m_pPlayer->FireBullets(18, vecSrc, vecAiming, Vector(0.25, 0.02, 0.25), 2048, BULLET_PLAYER_BUCKSHOT, 1);
 
 	if (pev->armorvalue == 0)
 		SendWeaponAnim(m_iClip == 0 ? SHOTGUN_SHOOT2_PUMP_EMPTY : SHOTGUN_SHOOT2_PUMP);
