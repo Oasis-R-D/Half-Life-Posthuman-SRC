7389655da74413e8981577b275b790491333e2ea
Pacman09 <pacmanproductionsz25@gmail.com>
2025-08-20 13:28:56 -0600

    add physbullet.h

--- dlls/crossbow_bolt.cpp
+++ dlls/crossbow_bolt.cpp
@@ -6,28 +6,12 @@
 #include "player.h"
 #include "gamerules.h"
 #include "UserMessages.h"
-
+#include "physical_bullet.h
 // UNDONE: Save/restore this?  Don't forget to set classname and LINK_ENTITY_TO_CLASS()
 //
 // OVERLOADS SOME ENTVARS:
 //
 // speed - the ideal magnitude of my velocity
-class CCrossbowBolt : public CBaseEntity
-{
-	void Spawn() override;
-	void Precache() override;
-	int Classify() override;
-	void EXPORT BubbleThink();
-	void EXPORT BoltTouch(CBaseEntity* pOther);
-	void EXPORT ExplodeThink();
-	void Stay();
-	int m_iTrail;
-
-public:
-	static CCrossbowBolt* BoltCreate();
-};
-LINK_ENTITY_TO_CLASS(crossbow_bolt, CCrossbowBolt);
-
 CCrossbowBolt* CCrossbowBolt::BoltCreate()
 {
 	// Create a new entity with CCrossbowBolt private data
