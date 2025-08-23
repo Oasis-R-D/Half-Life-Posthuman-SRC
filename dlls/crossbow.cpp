892e0334d4a4995405ee5882eabe4686e92a0ac6
Pacman09 <pacmanproductionsz25@gmail.com>
2025-08-23 16:08:00 -0600

    2 second RC reload

--- dlls/crossbow.cpp
+++ dlls/crossbow.cpp
@@ -245,7 +245,7 @@ void CCrossbow::Reload()
 		SecondaryAttack();
 	}
 
-	if (DefaultReload(1, CROSSBOW_RELOAD, 4.5))
+	if (DefaultReload(1, CROSSBOW_RELOAD, 2))
 	{
 		EMIT_SOUND_DYN(ENT(m_pPlayer->pev), CHAN_ITEM, "weapons/xbow_reload1.wav", RANDOM_FLOAT(0.95, 1.0), ATTN_NORM, 0, 93 + RANDOM_LONG(0, 0xF));
 	}
