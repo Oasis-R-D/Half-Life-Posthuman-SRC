5e6ffdfc113320cd6c249ddba54ffbc8ae29e8bb
Pacman09 <pacmanproductionsz25@gmail.com>
2025-08-27 13:50:04 -0600

    barnacle realism health balancing

--- dlls/barnacle.cpp
+++ dlls/barnacle.cpp
@@ -111,8 +111,16 @@ void CBarnacle::Spawn()
 	pev->movetype = MOVETYPE_NONE;
 	pev->takedamage = DAMAGE_AIM;
 	m_bloodColor = BLOOD_COLOR_RED;
-	pev->effects = EF_INVLIGHT; // take light from the ceiling
-	pev->health = 25;
+	pev->effects = EF_INVLIGHT; // take light from the ceiling\
+	if (g_iSkillLevel != SKILL_HARD)
+	{
+		pev->health = 25;
+	}
+	else
+	{
+		pev->health = 75;
+	}
+
 	m_flFieldOfView = 0.5; // indicates the width of this monster's forward view cone ( as a dotproduct result )
 	m_MonsterState = MONSTERSTATE_NONE;
 	m_flKillVictimTime = 0;
