9ba71c65106d5884eecc43059102153f97fb623e
Pacman09 <pacmanproductionsz25@gmail.com>
2025-09-17 20:10:56 -0600

    finish mons_target

--- dlls/info_monstarget.cpp
+++ dlls/info_monstarget.cpp
@@ -37,7 +37,7 @@ public:
 	int m_hitamnt;
 	int m_maxhitamnt;
 	void Killed(entvars_t* pevAttacker, int iGib) override;
-
+	void StartMonster() override; // makes it not scream about the enemy being in a wall
 	// No range attacks
 	bool CheckRangeAttack1(float flDot, float flDist) override { return false; }
 	bool CheckRangeAttack2(float flDot, float flDist) override { return false; }
@@ -106,7 +106,7 @@ void CTarget::Spawn()
 	SET_MODEL(ENT(pev), "models/target.mdl");
 	UTIL_SetSize(pev, VEC_HUMAN_HULL_MIN, VEC_HUMAN_HULL_MAX);
     pev->yaw_speed = 0;
-	pev->solid = SOLID_SLIDEBOX;
+	pev->solid = SOLID_BBOX;
 	pev->movetype = MOVETYPE_NONE;
 	m_bloodColor = DONT_BLEED;
 	pev->health = 9999;
@@ -124,4 +124,49 @@ void CTarget::Precache()
 	PRECACHE_MODEL("models/target.mdl");
 
 }
+void CTarget::StartMonster()
+{
+	if (!FStringNull(pev->target)) // this monster has a target
+	{
+		// Find the monster's initial target entity, stash it
+		m_pGoalEnt = CBaseEntity::Instance(FIND_ENTITY_BY_TARGETNAME(NULL, STRING(pev->target)));
 
+		if (!m_pGoalEnt)
+		{
+			ALERT(at_error, "ReadyMonster()--%s couldn't find target %s", STRING(pev->classname), STRING(pev->target));
+		}
+		else
+		{
+			// Monster will start turning towards his destination
+
+			// set the monster up to walk a path corner path.
+			// !!!BUGBUG - this is a minor bit of a hack.
+			// JAYJAY
+			m_movementGoal = MOVEGOAL_PATHCORNER;
+
+			if (pev->movetype == MOVETYPE_FLY)
+				m_movementActivity = ACT_FLY;
+			else
+				m_movementActivity = ACT_WALK;
+
+			SetState(MONSTERSTATE_IDLE);
+			ChangeSchedule(GetScheduleOfType(SCHED_IDLE_WALK));
+		}
+	}
+
+	//SetState ( m_IdealMonsterState );
+	//SetActivity ( m_IdealActivity );
+
+	// Delay drop to floor to make sure each door in the level has had its chance to spawn
+	// Spread think times so that they don't all happen at the same time (Carmack)
+	SetThink(&CBaseMonster::CallMonsterThink);
+	pev->nextthink += RANDOM_FLOAT(0.1, 0.4); // spread think times.
+
+	if (!FStringNull(pev->targetname)) // wait until triggered
+	{
+		SetState(MONSTERSTATE_IDLE);
+		// UNDONE: Some scripted sequence monsters don't have an idle?
+		SetActivity(ACT_IDLE);
+		ChangeSchedule(GetScheduleOfType(SCHED_WAIT_TRIGGER));
+	}
+}
