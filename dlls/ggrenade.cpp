8670d7164821576ec396b0043aa861c6267ee2a3
Pacman09 <pacmanproductionsz25@gmail.com>
2025-09-18 22:02:55 -0600

    realism shrapnel balancing

--- dlls/ggrenade.cpp
+++ dlls/ggrenade.cpp
@@ -210,7 +210,14 @@ void CGrenade::Detonate()
 	vecSpot = pev->origin + Vector(0, 0, 8);
 	UTIL_TraceLine(vecSpot, vecSpot + Vector(0, 0, -40), ignore_monsters, ENT(pev), &tr);
 	#ifndef CLIENT_DLL
-	CPhysbullet::BulletCreate(36, gSkillData.monDmg9MM, 3500, pev->origin, VECTOR_CONE_20DEGREES, 6.283, 6.283, 1, 12, edict());
+	if (g_iSkillLevel != SKILL_HARD)
+	{
+		CPhysbullet::BulletCreate(36, gSkillData.monDmg9MM, 5000, pev->origin, VECTOR_CONE_20DEGREES, 6.283, 6.283, 1, 12, edict());
+	}
+	else
+	{
+		CPhysbullet::BulletCreate(36, 20, 5000, pev->origin, VECTOR_CONE_20DEGREES, 6.283, 6.283, 1, 12, edict());
+	}
 	#endif
 	Explode(&tr, DMG_BLAST);
 }
@@ -229,7 +236,14 @@ void CGrenade::ExplodeTouch(CBaseEntity* pOther)
 	vecSpot = pev->origin - pev->velocity.Normalize() * 32;
 	UTIL_TraceLine(vecSpot, vecSpot + pev->velocity.Normalize() * 64, ignore_monsters, ENT(pev), &tr);
 	#ifndef CLIENT_DLL
-	CPhysbullet::BulletCreate(36, gSkillData.monDmg9MM, 3500, pev->origin, VECTOR_CONE_20DEGREES, 6.283, 6.283, 1, 12, edict());
+	if (g_iSkillLevel != SKILL_HARD)
+	{
+		CPhysbullet::BulletCreate(36, gSkillData.monDmg9MM, 5000, pev->origin, VECTOR_CONE_20DEGREES, 6.283, 6.283, 1, 12, edict());
+	}
+	else
+	{
+		CPhysbullet::BulletCreate(36, 20, 5000, pev->origin, VECTOR_CONE_20DEGREES, 6.283, 6.283, 1, 12, edict());
+	}
 	#endif
 	Explode(&tr, DMG_BLAST);
 }
