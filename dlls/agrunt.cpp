a48a7e709efa940c6221ea85c8dada2dff52c36d
Pacman09 <pacmanproductionsz25@gmail.com>
2025-09-24 13:56:23 -0600

    fix error

--- dlls/agrunt.cpp
+++ dlls/agrunt.cpp
@@ -266,10 +266,10 @@ void CAGrunt::TraceAttack(entvars_t* pevAttacker, float flDamage, Vector vecDir,
 		int BLDAMNT;
 
 		BLDAMNT = round(flDamage / 2);
-		SpawnBlood(ptr->vecEndPos, m_iBloodColor, flDamage); // a little surface blood.
+		SpawnBlood(ptr->vecEndPos, m_bloodColor, flDamage); // a little surface blood.
 		TraceBleed(flDamage, vecDir, ptr, bitsDamageType);
 		#ifndef CLIENT_DLL
-				CPhysblood::BloodCreate(BLDAMNT, 350, vecOrigin, vecDir, 1, m_iBloodColor);
+				CPhysblood::BloodCreate(BLDAMNT, 350, vecOrigin, vecDir, 1, m_bloodColor);
 		#endif
 	}
 
