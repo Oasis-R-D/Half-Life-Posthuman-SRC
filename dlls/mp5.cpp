f6c7da2ceae2b0e072b538b1451596765d4a59bf
PackMan09 <pacmanproductionsz25@gmail.com>
2025-09-26 10:55:40 -0600

    (DNC) define function

--- dlls/mp5.cpp
+++ dlls/mp5.cpp
@@ -648,12 +648,11 @@ void CM727::SecondaryAttack()
 					break;
 				}
 				m_pPlayer->m_iWeaponVolume = CROWBAR_BODYHIT_VOLUME;
-				if (!pEntity->IsAlive())
-					return true;
-				else
+				if (pEntity->IsAlive())
+				{
 					flVol = 0.1;
-
-				fHitWorld = false;
+					fHitWorld = false;
+				}
 			}
 		}
 
@@ -749,3 +748,47 @@ class CM727AmmoClip : public CBasePlayerAmmo
 	}
 };
 LINK_ENTITY_TO_CLASS(ammo_556mag, CM727AmmoClip);
+
+void FindHullIntersection(const Vector& vecSrc, TraceResult& tr, const Vector& mins, const Vector& maxs, edict_t* pEntity)
+{
+	int i, j, k;
+	float distance;
+	const Vector* minmaxs[2] = {&mins, &maxs};
+	TraceResult tmpTrace;
+	Vector vecHullEnd = tr.vecEndPos;
+	Vector vecEnd;
+
+	distance = 1e6f;
+
+	vecHullEnd = vecSrc + ((vecHullEnd - vecSrc) * 2);
+	UTIL_TraceLine(vecSrc, vecHullEnd, dont_ignore_monsters, pEntity, &tmpTrace);
+	if (tmpTrace.flFraction < 1.0)
+	{
+		tr = tmpTrace;
+		return;
+	}
+
+	for (i = 0; i < 2; i++)
+	{
+		for (j = 0; j < 2; j++)
+		{
+			for (k = 0; k < 2; k++)
+			{
+				vecEnd.x = vecHullEnd.x + minmaxs[i]->x;
+				vecEnd.y = vecHullEnd.y + minmaxs[j]->y;
+				vecEnd.z = vecHullEnd.z + minmaxs[k]->z;
+
+				UTIL_TraceLine(vecSrc, vecEnd, dont_ignore_monsters, pEntity, &tmpTrace);
+				if (tmpTrace.flFraction < 1.0)
+				{
+					float thisDistance = (tmpTrace.vecEndPos - vecSrc).Length();
+					if (thisDistance < distance)
+					{
+						tr = tmpTrace;
+						distance = thisDistance;
+					}
+				}
+			}
+		}
+	}
+}
