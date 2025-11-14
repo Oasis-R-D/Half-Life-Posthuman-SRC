c80a4524f6305f1bc055fa9c05d508c90b060a53
Pacman09 <pacmanproductionsz25@gmail.com>
2025-11-14 16:19:45 -0700

    Remove PHYSbloodspray spawn offset

--- dlls/Blooddrops.cpp
+++ dlls/Blooddrops.cpp
@@ -110,7 +110,7 @@ void CPhysblood::Spawn()
 	}
 	pev->movetype = MOVETYPE_TOSS; // makes it have gravity
 	pev->solid = SOLID_BBOX;
-	UTIL_SetOrigin(pev, m_SpawnPos + (m_direction * 24) * m_opposite); //spawn a little bit more forward
+	UTIL_SetOrigin(pev, m_SpawnPos);
 	pev->velocity = ((m_direction + RANDOM_VECTOR(-m_Spread, m_Spread)) * m_BloodDropVel) * m_opposite; // Applies spread and velocity, also applies the chance to have the entry wound droplets
 	pev->gravity = m_Gravity;
 	pev->owner = NULL;
@@ -302,8 +302,6 @@ int CPhysblood::ShouldCollide(CBaseEntity* pentTouched)
 	if (pentTouched->IsBSPModel())
 		return 1;
 	else
-	{
 		return 0;
-	}
 }
 #endif
