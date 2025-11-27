3eef8423d7d8e965de465150da97125359bfcf3a
Pacman09 <pacmanproductionsz25@gmail.com>
2025-11-27 00:53:07 -0700

    add to-do comment

--- cl_dll/view.cpp
+++ cl_dll/view.cpp
@@ -752,7 +752,7 @@ void V_CalcNormalRefdef(struct ref_params_s* pparams)
 	// pushing the view origin down off of the same X/Z plane as the ent's origin will give the
 	// gun a very nice 'shifting' effect when the player looks up/down. If there is a problem
 	// with view model distortion, this may be a cause. (SJB).
-	view->origin[2] -= 1;
+	view->origin[2] -= 1; // TO-DO: this is probably what is breaking stuff
 
 	// fudge position around to keep amount of weapon visible
 	// roughly equal with different FOV
