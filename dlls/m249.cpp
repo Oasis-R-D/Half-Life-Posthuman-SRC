fe3e552447fba47bd2084d1eca3a7df5459c0e85
Pacman09 <pacmanproductionsz25@gmail.com>
2026-01-16 14:56:35 -0700

    Change m249 crosshair type

--- dlls/m249.cpp
+++ dlls/m249.cpp
@@ -55,7 +55,7 @@ void CM249::Spawn()
 
 bool CM249::Deploy()
 {
-	m_iCrossHairType = CROSSHAIR_NOCENTER;
+	m_iCrossHairType = CROSSHAIR_DEFAULT;
 	m_flAccuracyPenalty = 20 * M249_ACCURACY_SHOT_PENALTY_TIME;
 	m_pPlayer->pev->maxspeed = 192;
 	return DefaultDeploy("models/v_saw.mdl", "models/p_saw.mdl", M249_DRAW, "mp5");
